7e666a9b99818d4c3edcd9ab4a5a9025b259364c
Rahul Gupta <rahulguptagzb09@gmail.com>
2025-09-30 09:50:28 +0530

    added 1 python solution for 148

---
+++ medium/medium-0148-sort-list.py
@@ -0,0 +1,64 @@
+"""
+https://leetcode.com/problems/sort-list/description/
+148. Sort List
+Given the head of a linked list, return the list after sorting it in ascending order.
+Example 1:
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+Input: head = []
+Output: []
+Constraints:
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+"""
+# Time - O(nlogn)
+# Space - O(1)
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        # split list into 2 halves
+        left = head
+        right = self.getMid(head)
+        tmp = right.next
+        right.next = None
+        right = tmp
+        left = self.sortList(left)
+        right = self.sortList(right)
+        return self.merge(left, right)
+
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def merge(self, left, right):
+        tail = dummy = ListNode()
+        while left and right:
+            if left.val < right.val:
+                tail.next = left
+                left = left.next
+            else:
+                tail.next = right
+                right = right.next
+            tail = tail.next
+        if left:
+            tail.next = left
+        if right:
+            tail.next = right
+        return dummy.next
