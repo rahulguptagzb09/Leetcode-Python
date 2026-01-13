453384c0e02f0df6294ba8b630e957f52b2f327b
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-01-13 10:13:19 +0530

    added 1 python solution for 658

---
+++ medium/medium-0658-find-k-closest-elements.py
@@ -0,0 +1,39 @@
+"""
+https://leetcode.com/problems/find-k-closest-elements/description/
+658. Find K Closest Elements
+Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+An integer a is closer to x than an integer b if:
+|a - x| < |b - x|, or
+|a - x| == |b - x| and a < b
+Example 1:
+Input: arr = [1,2,3,4,5], k = 4, x = 3
+Output: [1,2,3,4]
+Example 2:
+Input: arr = [1,1,2,3,4,5], k = 4, x = -1
+Output: [1,1,2,3]
+Constraints:
+1 <= k <= arr.length
+1 <= arr.length <= 104
+arr is sorted in ascending order.
+-104 <= arr[i], x <= 104
+"""
+# Time - O(logn + k)
+# Space - O(1)
+
+from typing import List
+
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, len(arr) - k
+        while l < r:
+            m = l + (r - l) // 2
+            if x - arr[m] > arr[m + k] - x:
+                l = m + 1
+            else:
+                r = m
+        return arr[l:l + k]
+
+sol = Solution()
+print(sol.findClosestElements(arr = [1,2,3,4,5], k = 4, x = 3))
+print(sol.findClosestElements(arr = [1,1,2,3,4,5], k = 4, x = -1))
