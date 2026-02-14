7b14949e54112fcc580cba8caebc896c64c735b6
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-02-14 10:29:19 +0530

    added 1 python solution for 2226

---
+++ medium/medium-2226-maximum-candies-allocated-to-k-children.py
@@ -0,0 +1,54 @@
+"""
+https://leetcode.com/problems/maximum-candies-allocated-to-k-children/
+2226. Maximum Candies Allocated to K Children
+You are given a 0-indexed integer array candies. Each element in the array denotes a pile of candies of size candies[i]. You can divide each pile into any number of sub piles, but you cannot merge two piles together.
+You are also given an integer k. You should allocate piles of candies to k children such that each child gets the same number of candies. Each child can be allocated candies from only one pile of candies and some piles of candies may go unused.
+Return the maximum number of candies each child can get.
+Example 1:
+Input: candies = [5,8,6], k = 3
+Output: 5
+Explanation: We can divide candies[1] into 2 piles of size 5 and 3, and candies[2] into 2 piles of size 5 and 1. We now have five piles of candies of sizes 5, 5, 3, 5, and 1. We can allocate the 3 piles of size 5 to 3 children. It can be proven that each child cannot receive more than 5 candies.
+Example 2:
+Input: candies = [2,5], k = 11
+Output: 0
+Explanation: There are 11 children but only 7 candies in total, so it is impossible to ensure each child receives at least one candy. Thus, each child gets no candy and the answer is 0.
+Constraints:
+1 <= candies.length <= 105
+1 <= candies[i] <= 107
+1 <= k <= 1012
+Hint 1
+For a fixed number of candies c, how can you check if each child can get c candies?
+Hint 2
+Use binary search to find the maximum c as the answer.
+"""
+# Time - O(nlogn)
+# Space - O(1)
+
+from typing import List
+
+
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        total = sum(candies)
+        if total < k:
+            return 0
+        l, r = 1, total // k
+        res = 0
+        while l <= r:
+            m = l + (r - l) // 2
+            count = 0
+            for c in candies:
+                if c >= m:
+                    count += c // m
+                if count >= k:
+                    break
+            if count >= k:
+                res = m
+                l = m + 1
+            else:
+                r = m - 1
+        return res
+
+sol = Solution()
+print(sol.maximumCandies(candies = [5,8,6], k = 3))
+print(sol.maximumCandies(candies = [2,5], k = 11))
