219052068c96fa8d52d848d8d7c5f09008f891a9
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-02-16 11:16:38 +0530

    added 1 python solution for 2560

---
+++ medium/medium-2560-house-robber-iv.py
@@ -0,0 +1,70 @@
+"""
+https://leetcode.com/problems/house-robber-iv/
+2560. House Robber IV
+There are several consecutive houses along a street, each of which has some money inside. There is also a robber, who wants to steal money from the homes, but he refuses to steal from adjacent homes.
+The capability of the robber is the maximum amount of money he steals from one house of all the houses he robbed.
+You are given an integer array nums representing how much money is stashed in each house. More formally, the ith house from the left has nums[i] dollars.
+You are also given an integer k, representing the minimum number of houses the robber will steal from. It is always possible to steal at least k houses.
+Return the minimum capability of the robber out of all the possible ways to steal at least k houses.
+Example 1:
+Input: nums = [2,3,5,9], k = 2
+Output: 5
+Explanation: 
+There are three ways to rob at least 2 houses:
+- Rob the houses at indices 0 and 2. Capability is max(nums[0], nums[2]) = 5.
+- Rob the houses at indices 0 and 3. Capability is max(nums[0], nums[3]) = 9.
+- Rob the houses at indices 1 and 3. Capability is max(nums[1], nums[3]) = 9.
+Therefore, we return min(5, 9, 9) = 5.
+Example 2:
+Input: nums = [2,7,9,3,1], k = 2
+Output: 2
+Explanation: There are 7 ways to rob the houses. The way which leads to minimum capability is to rob the house at index 0 and 4. Return max(nums[0], nums[4]) = 2.
+Constraints:
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+1 <= k <= (nums.length + 1)/2
+Hint 1
+Can we use binary search to find the minimum value of a non-contiguous subsequence of a given size k?
+Hint 2
+Initialize the search range with the minimum and maximum elements of the input array.
+Hint 3
+Use a check function to determine if it is possible to select k non-consecutive elements that are less than or equal to the current "guess" value.
+Hint 4
+Adjust the search range based on the outcome of the check function, until the range converges and the minimum value is found.
+"""
+# Time - O(nlogm)
+# Space - O(1)
+
+from typing import List
+
+
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+
+        def is_valid(capability):
+            i = 0
+            count = 0
+            while i < len(nums):
+                if nums[i] <= capability:
+                    i += 2
+                    count += 1
+                else:
+                    i += 1
+                if count == k:
+                    break
+            return count == k
+
+        l, r = min(nums), max(nums)
+        res = 0
+        while l <= r:
+            m = l + (r - l) // 2
+            if is_valid(m):
+                res = m
+                r = m - 1
+            else:
+                l = m + 1
+        return res
+
+sol = Solution()
+print(sol.minCapability(nums = [2,3,5,9], k = 2))
+print(sol.minCapability(nums = [2,7,9,3,1], k = 2))
