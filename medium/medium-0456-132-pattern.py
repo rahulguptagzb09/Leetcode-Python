a51a82a04aec58873a40665f5a1af6ab29a99aa9
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-03-01 10:40:02 +0530

    added 1 python solution for 456

---
+++ medium/medium-0456-132-pattern.py
@@ -0,0 +1,45 @@
+"""
+https://leetcode.com/problems/132-pattern/
+456. 132 Pattern
+Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+Return true if there is a 132 pattern in nums, otherwise, return false.
+Example 1:
+Input: nums = [1,2,3,4]
+Output: false
+Explanation: There is no 132 pattern in the sequence.
+Example 2:
+Input: nums = [3,1,4,2]
+Output: true
+Explanation: There is a 132 pattern in the sequence: [1, 4, 2].
+Example 3:
+Input: nums = [-1,3,2,0]
+Output: true
+Explanation: There are three 132 patterns in the sequence: [-1, 3, 2], [-1, 3, 0] and [-1, 2, 0].
+Constraints:
+n == nums.length
+1 <= n <= 2 * 105
+-109 <= nums[i] <= 109
+"""
+# Time - O(n)
+# Space - O(n)
+
+from typing import List
+
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        stack = [] # pair [nums, min_left] mono decreasimg
+        cur_min = nums[0]
+        for n in nums[1:]:
+            while stack and n >= stack[-1][0]:
+                stack.pop()
+            if stack and n > stack[-1][1]:
+                return True
+            stack.append([n, cur_min])
+            cur_min = min(cur_min, n)
+        return False
+
+sol = Solution()
+print(sol.find132pattern(nums = [1,2,3,4]))
+print(sol.find132pattern(nums = [3,1,4,2]))
+print(sol.find132pattern(nums = [-1,3,2,0]))
