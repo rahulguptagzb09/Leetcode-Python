c3d3187d86b13c4bb8284e8d1fe3e49c53f2f41b
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-04-03 10:10:05 +0530

    added 1 python solution for 1762

---
+++ medium/medium-1762-buildings-with-an-ocean-view.py
@@ -0,0 +1,47 @@
+"""
+https://leetcode.com/problems/buildings-with-an-ocean-view/description/
+https://leetcode.ca/all/1762.html
+1762. Buildings With an Ocean View
+There are n buildings in a line. You are given an integer array heights of size n that represents the heights of the buildings in the line.
+The ocean is to the right of the buildings. A building has an ocean view if the building can see the ocean without obstructions. Formally, a building has an ocean view if all the buildings to its right have a smaller height.
+Return a list of indices (0-indexed) of buildings that have an ocean view, sorted in increasing order.
+Example 1:
+Input: heights = [4,2,3,1]
+Output: [0,2,3]
+Explanation: Building 1 (0-indexed) does not have an ocean view because building 2 is taller.
+Example 2:
+Input: heights = [4,3,2,1]
+Output: [0,1,2,3]
+Explanation: All the buildings have an ocean view.
+Example 3:
+Input: heights = [1,3,2,4]
+Output: [3]
+Explanation: Only building 3 has an ocean view.
+Example 4:
+Input: heights = [2,2,2,2]
+Output: [3]
+Explanation: Buildings cannot see the ocean if there are buildings of the same height to its right.
+Constraints:
+1 <= heights.length <= 105
+1 <= heights[i] <= 109
+"""
+# Time - O(n)
+# Space - O(1)
+
+from typing import List
+
+
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        res = [len(heights) - 1]
+        for i in range(len(heights) - 2, -1, -1):
+            if heights[i] > heights[res[-1]]:
+                res.append(i)
+        res.reverse()
+        return res
+
+sol = Solution()
+print(sol.findBuildings(heights = [4,2,3,1]))
+print(sol.findBuildings(heights = [4,3,2,1]))
+print(sol.findBuildings(heights = [1,3,2,4]))
+print(sol.findBuildings(heights = [2,2,2,2]))
