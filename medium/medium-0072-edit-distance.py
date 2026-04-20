3348c9d17e44e594faae56a55824fdd07bf48c9f
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-04-20 10:04:54 +0530

    added 1 python solution for 72

--- medium/medium-0072-edit-distance.py
+++ medium/medium-0072-edit-distance.py
@@ -1,5 +1,5 @@
 """
-https://leetcode.com/problems/edit-distance/description/
+https://leetcode.com/problems/edit-distance/
 72. Edit Distance
 Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
 You have the following three operations permitted on a word:
@@ -9,14 +9,14 @@ Replace a character
 Example 1:
 Input: word1 = "horse", word2 = "ros"
 Output: 3
-Explanation: 
+Explanation:
 horse -> rorse (replace 'h' with 'r')
 rorse -> rose (remove 'r')
 rose -> ros (remove 'e')
 Example 2:
 Input: word1 = "intention", word2 = "execution"
 Output: 5
-Explanation: 
+Explanation:
 intention -> inention (remove 't')
 inention -> enention (replace 'i' with 'e')
 enention -> exention (replace 'n' with 'x')
@@ -26,24 +26,28 @@ Constraints:
 0 <= word1.length, word2.length <= 500
 word1 and word2 consist of lowercase English letters.
 """
+
 # Time - O(n*m)
 # Space - O(n*m)
 
+
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        cache = [ [float("inf")] * (len(word2) + 1) for _ in range(len(word1) + 1) ]
+        # Dynamic Programming Bottom Up
+        dp = [[float("inf")] * (len(word2) + 1) for _ in range(len(word1) + 1)]
         for j in range(len(word2) + 1):
-            cache[len(word1)][j] = len(word2) - j
+            dp[len(word1)][j] = len(word2) - j
         for i in range(len(word1) + 1):
-            cache[i][len(word2)] = len(word1) - i
+            dp[i][len(word2)] = len(word1) - i
         for i in range(len(word1) - 1, -1, -1):
             for j in range(len(word2) - 1, -1, -1):
                 if word1[i] == word2[j]:
-                    cache[i][j] = cache[i + 1][j + 1]
+                    dp[i][j] = dp[i + 1][j + 1]
                 else:
-                    cache[i][j] = 1 + min(cache[i + 1][j], cache[i][j + 1], cache[i + 1][j + 1])
-        return cache[0][0]
+                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
+        return dp[0][0]
+
 
 sol = Solution()
-print(sol.minDistance(word1 = "horse", word2 = "ros"))
-print(sol.minDistance(word1 = "intention", word2 = "execution"))
+print(sol.minDistance(word1="horse", word2="ros"))
+print(sol.minDistance(word1="intention", word2="execution"))
