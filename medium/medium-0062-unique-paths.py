b6436e227dea6ea549a7ca805bbed4d3aa95abbd
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-04-24 12:57:30 +0530

    added 1 python solution for 62

--- medium/medium-0062-unique-paths.py
+++ medium/medium-0062-unique-paths.py
@@ -1,5 +1,5 @@
 """
-https://leetcode.com/problems/unique-paths/description/
+https://leetcode.com/problems/unique-paths/
 62. Unique Paths
 There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
 Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
@@ -7,7 +7,7 @@ The test cases are generated so that the answer will be less than or equal to 2 
 Example 1:
 Input: m = 3, n = 7
 Output: 28
-Example 2:
+Example 2:62
 Input: m = 3, n = 2
 Output: 3
 Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
@@ -17,8 +17,10 @@ Explanation: From the top-left corner, there are a total of 3 ways to reach the 
 Constraints:
 1 <= m, n <= 100
 """
+
 # Time - O(n*m)
 # Space - O(n)
+
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
@@ -30,6 +32,7 @@ class Solution:
             row = new_row
         return row[0]
 
+
 sol = Solution()
-print(sol.uniquePaths(m = 3, n = 7))
-print(sol.uniquePaths(m = 3, n = 2))
+print(sol.uniquePaths(m=3, n=7))
+print(sol.uniquePaths(m=3, n=2))
