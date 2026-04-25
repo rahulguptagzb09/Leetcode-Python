3e23906ac5eef6c98396486b320675356282376e
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-04-25 17:27:10 +0530

    added 1 python solution for 57

--- medium/medium-0057-insert-interval.py
+++ medium/medium-0057-insert-interval.py
@@ -1,5 +1,5 @@
 """
-https://leetcode.com/problems/insert-interval/description/
+https://leetcode.com/problems/insert-interval/
 57. Insert Interval
 You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
 Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
@@ -26,26 +26,34 @@ Can you try merging the overlapping intervals while inserting the new interval?
 Hint 3
 This can be done by comparing the end of the last interval with the start of the new interval and vice versa.
 """
+
 # Time - O(n)
-# Space - O(n)
+# Space - O(1)
 
 from typing import List
 
 
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    def insert(
+        self, intervals: List[List[int]], newInterval: List[int]
+    ) -> List[List[int]]:
         res = []
-        for i in range(len(intervals)):
-            if newInterval[1] < intervals[i][0]:
+        for i, inv in enumerate(intervals):
+            if newInterval[1] < inv[0]:
                 res.append(newInterval)
                 return res + intervals[i:]
-            elif newInterval[0] > intervals[i][1]:
-                res.append(intervals[i])
+            elif newInterval[0] > inv[1]:
+                res.append(inv)
             else:
-                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+                newInterval = [min(newInterval[0], inv[0]), max(newInterval[1], inv[1])]
         res.append(newInterval)
         return res
 
+
 sol = Solution()
-print(sol.insert(intervals = [[1,3],[6,9]], newInterval = [2,5]))
-print(sol.insert(intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]))
+print(sol.insert(intervals=[[1, 3], [6, 9]], newInterval=[2, 5]))
+print(
+    sol.insert(
+        intervals=[[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], newInterval=[4, 8]
+    )
+)
