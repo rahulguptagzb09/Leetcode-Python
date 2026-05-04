42d6ede2f23ceb72dee1774fe308b2e5461643a0
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-05-04 10:37:43 +0530

    added 1 python solution for 48

--- medium/medium-0048-rotate-image.py
+++ medium/medium-0048-rotate-image.py
@@ -1,5 +1,5 @@
 """
-https://leetcode.com/problems/rotate-image/description/
+https://leetcode.com/problems/rotate-image/
 48. Rotate Image
 You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
 You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
@@ -14,6 +14,7 @@ n == matrix.length == matrix[i].length
 1 <= n <= 20
 -1000 <= matrix[i][j] <= 1000
 """
+
 # Time - O(n^2)
 # Space - O(1)
 
@@ -25,12 +26,13 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        # Math and Geometry
         l, r = 0, len(matrix) - 1
         while l < r:
             for i in range(r - l):
                 top, bottom = l, r
                 # save the top left
-                top_left = matrix[top][l + i] 
+                top_left = matrix[top][l + i]
                 # move bottom left into top left
                 matrix[top][l + i] = matrix[bottom - i][l]
                 # move bottom right into bottom left
@@ -43,6 +45,9 @@ class Solution:
             l += 1
         return matrix
 
+
 sol = Solution()
-print(sol.rotate(matrix = [[1,2,3],[4,5,6],[7,8,9]]))
-print(sol.rotate(matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]))
+print(sol.rotate(matrix=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+print(
+    sol.rotate(matrix=[[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]])
+)
