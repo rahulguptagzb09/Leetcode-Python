ef32e1ddd4117cc3a0a4e00ce9a0bc987957eb1b
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-06-07 11:22:12 +0530

    added 1 python solution for 2616

--- medium/medium-2616-minimize-maximum-difference-pairs.py
+++ medium/medium-2616-minimize-maximum-difference-pairs.py
@@ -7,7 +7,7 @@ Return the minimum maximum difference among all p pairs. We define the maximum o
 Example 1:
 Input: nums = [10,1,2,7,1,3], p = 2
 Output: 1
-Explanation: The first pair is formed from the indices 1 and 4, and the second pair is formed from the indices 2 and 5. 
+Explanation: The first pair is formed from the indices 1 and 4, and the second pair is formed from the indices 2 and 5.
 The maximum difference is max(|nums[1] - nums[4]|, |nums[2] - nums[5]|) = max(0, 1) = 1. Therefore, we return 1.
 Example 2:
 Input: nums = [4,2,1,2], p = 1
@@ -24,17 +24,18 @@ To minimize the answer, the array should be sorted first.
 Hint 3
 The recurrence relation is fn(i, x) = min(fn(i+1, x), max(abs(nums[i]-nums[i+1]), fn(i+2, p-1)), and fn(0,p) gives the desired answer.
 """
+
 # Time - O(nlogm)
-# Space = O(nlogm)
+# Space - O(nlogm)
 
 from typing import List
 
 
 def minimizeMax(nums: List[int], p: int) -> int:
-    
+
     def isValid(threshold):
         # Greedy
-        i, cnt = 0, 0 
+        i, cnt = 0, 0
         while i < len(nums) - 1:
             if abs(nums[i] - nums[i + 1]) <= threshold:
                 cnt += 1
@@ -51,13 +52,14 @@ def minimizeMax(nums: List[int], p: int) -> int:
     l, r = 0, 10**9
     res = 10**9
     while l <= r:
-        m = l + (r - l) // 2 # (l + r) // 2
+        m = l + (r - l) // 2  # (l + r) // 2
         if isValid(m):
-            res = m 
+            res = m
             r = m - 1
         else:
             l = m + 1
     return res
 
-print(minimizeMax(nums = [10,1,2,7,1,3], p = 2))
-print(minimizeMax(nums = [4,2,1,2], p = 1))
+
+print(minimizeMax(nums=[10, 1, 2, 7, 1, 3], p=2))
+print(minimizeMax(nums=[4, 2, 1, 2], p=1))
