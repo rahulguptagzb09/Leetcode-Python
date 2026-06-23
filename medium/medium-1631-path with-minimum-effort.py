6b2302b12f915521e3a8a18d2eff999964401e15
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-06-23 10:11:45 +0530

    added 1 python solution for 1631

--- medium/medium-1631-path with-minimum-effort.py
+++ medium/medium-1631-path with-minimum-effort.py
@@ -1,5 +1,5 @@
 """
-https://leetcode.com/problems/path-with-minimum-effort/description/
+https://leetcode.com/problems/path-with-minimum-effort/
 1631. Path With Minimum Effort
 You are a hiker preparing for an upcoming hike. You are given heights, a 2D array of size rows x columns, where heights[row][col] represents the height of cell (row, col). You are situated in the top-left cell, (0, 0), and you hope to travel to the bottom-right cell, (rows-1, columns-1) (i.e., 0-indexed). You can move up, down, left, or right, and you wish to find a route that requires the minimum effort.
 A route's effort is the maximum absolute difference in heights between two consecutive cells of the route.
@@ -29,6 +29,7 @@ If you are given threshold k, check if it is possible to go from (0, 0) to (n-1,
 Hint 3
 Binary search the k value.
 """
+
 # Time - O(n*m * 2^lognm)
 # Space - O(n*m)
 
@@ -36,25 +37,44 @@ import heapq
 from typing import List
 
 
-def minimumEffortPath(heights: List[List[int]]) -> int:
-    ROWS, COLS = len(heights), len(heights[0])
-    minHeap = [[0, 0, 0]] # [diff, r, c]
-    visit = set()
-    directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-    while minHeap:
-        diff, r, c = heapq.heappop(minHeap)
-        if (r, c) in visit:
-            continue
-        visit.add((r, c))
-        if (r, c) == (ROWS - 1, COLS - 1):
-            return diff
-        for dr, dc in directions:
-            newR, newC = r + dr, c + dc
-            if (newR < 0 or newC < 0 or newR == ROWS or newC == COLS) or (newR, newC) in visit:
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        rows, cols = len(heights), len(heights[0])
+        min_heap = [[0, 0, 0]]  # [diff, r, c]
+        visit = set()
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        while min_heap:
+            diff, r, c = heapq.heappop(min_heap)
+            if (r, c) in visit:
                 continue
-            newDiff = max(diff, abs(heights[r][c] - heights[newR][newC]))
-            heapq.heappush(minHeap, [newDiff, newR, newC])
+            visit.add((r, c))
+            if (r, c) == (rows - 1, cols - 1):
+                return diff
+            for dr, dc in directions:
+                new_r, new_c = r + dr, c + dc
+                if (
+                    new_r < 0
+                    or new_c < c
+                    or new_r == rows
+                    or new_c == cols
+                    or (new_r, new_c) in visit
+                ):
+                    continue
+                new_diff = max(diff, abs(heights[r][c] - heights[new_r][new_c]))
+                heapq.heappush(min_heap, (new_diff, new_r, new_c))
 
-print(minimumEffortPath(heights = [[1,2,2],[3,8,2],[5,3,5]]))
-print(minimumEffortPath(heights = [[1,2,3],[3,8,4],[5,3,5]]))
-print(minimumEffortPath(heights = [[1,2,1,1,1],[1,2,1,2,1],[1,2,1,2,1],[1,2,1,2,1],[1,1,1,2,1]]))
+
+sol = Solution()
+print(sol.minimumEffortPath(heights=[[1, 2, 2], [3, 8, 2], [5, 3, 5]]))
+print(sol.minimumEffortPath(heights=[[1, 2, 3], [3, 8, 4], [5, 3, 5]]))
+print(
+    sol.minimumEffortPath(
+        heights=[
+            [1, 2, 1, 1, 1],
+            [1, 2, 1, 2, 1],
+            [1, 2, 1, 2, 1],
+            [1, 2, 1, 2, 1],
+            [1, 1, 1, 2, 1],
+        ]
+    )
+)
