f35f492bb245aa4c5cf2a14762b6abe281c87cad
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-07-14 09:52:14 +0530

    added 1 python solution for 1405

---
+++ medium/medium-1405-longest-happy-string.py
@@ -0,0 +1,62 @@
+"""
+https://leetcode.com/problems/longest-happy-string/description/
+1405. Longest Happy String
+A string s is called happy if it satisfies the following conditions:
+s only contains the letters 'a', 'b', and 'c'.
+s does not contain any of "aaa", "bbb", or "ccc" as a substring.
+s contains at most a occurrences of the letter 'a'.
+s contains at most b occurrences of the letter 'b'.
+s contains at most c occurrences of the letter 'c'.
+Given three integers a, b, and c, return the longest possible happy string. If there are multiple longest happy strings, return any of them. If there is no such string, return the empty string "".
+A substring is a contiguous sequence of characters within a string.
+Example 1:
+Input: a = 1, b = 1, c = 7
+Output: "ccaccbcc"
+Explanation: "ccbccacc" would also be a correct answer.
+Example 2:
+Input: a = 7, b = 1, c = 0
+Output: "aabaa"
+Explanation: It is the only correct answer in this case.
+Constraints:
+0 <= a, b, c <= 100
+a + b + c > 0
+Hint 1
+Use a greedy approach.
+Hint 2
+Use the letter with the maximum current limit that can be added without breaking the condition.
+"""
+
+# Time - O(n)
+# Space - O(1)
+
+import heapq
+
+
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        res = ""
+        max_heap = []
+        for count, char in [(-a, "a"), (-b, "b"), (-c, "c")]:
+            if count != 0:
+                heapq.heappush(max_heap, (count, char))
+        while max_heap:
+            count, char = heapq.heappop(max_heap)
+            if len(res) > 1 and res[-1] == res[-2] == char:
+                if not max_heap:
+                    break
+                count2, char2 = heapq.heappop(max_heap)
+                res += char2
+                count2 += 1
+                if count2:
+                    heapq.heappush(max_heap, (count2, char2))
+            else:
+                res += char
+                count += 1
+            if count:
+                heapq.heappush(max_heap, (count, char))
+        return res
+
+
+sol = Solution()
+print(sol.longestDiverseString(a=1, b=1, c=7))
+print(sol.longestDiverseString(a=7, b=1, c=0))
