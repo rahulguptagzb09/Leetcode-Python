de90bb2cc6f29557a974da69e59983f97c299142
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-07-18 10:22:00 +0530

    added 1 python solution for 1143

--- medium/medium-1143-longest-common-subsequence.py
+++ medium/medium-1143-longest-common-subsequence.py
@@ -1,13 +1,13 @@
 """
-https://leetcode.com/problems/longest-common-subsequence/description/
+https://leetcode.com/problems/longest-common-subsequence/
 1143. Longest Common Subsequence
 Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
 A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
 For example, "ace" is a subsequence of "abcde".
 A common subsequence of two strings is a subsequence that is common to both strings.
 Example 1:
-Input: text1 = "abcde", text2 = "ace" 
-Output: 3  
+Input: text1 = "abcde", text2 = "ace"
+Output: 3
 Explanation: The longest common subsequence is "ace" and its length is 3.
 Example 2:
 Input: text1 = "abc", text2 = "abc"
@@ -25,21 +25,24 @@ Try dynamic programming. DP[i][j] represents the longest common subsequence of t
 Hint 2
 DP[i][j] = DP[i - 1][j - 1] + 1 , if text1[i] == text2[j] DP[i][j] = max(DP[i - 1][j], DP[i][j - 1]) , otherwise
 """
+
 # Time - O(n*m)
 # Space - O(n*m)
 
+
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
-        for i in range(len(text1) - 1, -1, -1):
-            for j in range(len(text2) - 1, -1, -1):
-                if text1[i] == text2[j]:
-                    dp[i][j] = 1 + dp[i + 1][j + 1]
+        dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+        for r in range(len(text1) - 1, -1, -1):
+            for c in range(len(text2) - 1, -1, -1):
+                if text1[r] == text2[c]:
+                    dp[r][c] = 1 + dp[r + 1][c + 1]
                 else:
-                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+                    dp[r][c] = max(dp[r][c + 1], dp[r + 1][c])
         return dp[0][0]
 
+
 sol = Solution()
-print(sol.longestCommonSubsequence(text1 = "abcde", text2 = "ace"))
-print(sol.longestCommonSubsequence(text1 = "abc", text2 = "abc"))
-print(sol.longestCommonSubsequence(text1 = "abc", text2 = "def"))
+print(sol.longestCommonSubsequence(text1="abcde", text2="ace"))
+print(sol.longestCommonSubsequence(text1="abc", text2="abc"))
+print(sol.longestCommonSubsequence(text1="abc", text2="def"))
