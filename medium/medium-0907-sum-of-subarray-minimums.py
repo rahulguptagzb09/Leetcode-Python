d28dca807dba87a19b766c0675556caf4d9f6f88
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-07-28 09:41:04 +0530

    added 1 python solution for 907

--- medium/medium-0907-sum-of-subarray-minimums.py
+++ medium/medium-0907-sum-of-subarray-minimums.py
@@ -5,8 +5,8 @@ Given an array of integers arr, find the sum of min(b), where b ranges over ever
 Example 1:
 Input: arr = [3,1,2,4]
 Output: 17
-Explanation: 
-Subarrays are [3], [1], [2], [4], [3,1], [1,2], [2,4], [3,1,2], [1,2,4], [3,1,2,4]. 
+Explanation:
+Subarrays are [3], [1], [2], [4], [3,1], [1,2], [2,4], [3,1,2], [1,2,4], [3,1,2,4].
 Minimums are 3, 1, 2, 4, 1, 1, 2, 1, 1, 1.
 Sum is 17.
 Example 2:
@@ -16,8 +16,9 @@ Constraints:
 1 <= arr.length <= 3 * 104
 1 <= arr[i] <= 3 * 104
 """
+
 # Time - O(n)
-# Space  - O(n)
+# Space - O(n)
 
 from typing import List
 
@@ -25,7 +26,7 @@ from typing import List
 def sumSubarrayMins(arr: List[int]) -> int:
     MOD = 10**9 + 7
     res = 0
-    stack = [] # (index, num)
+    stack = []  # (index, num)
     for i, n in enumerate(arr):
         while stack and n < stack[-1][1]:
             j, m = stack.pop()
@@ -33,7 +34,7 @@ def sumSubarrayMins(arr: List[int]) -> int:
             right = i - j
             res = (res + m * left * right) % MOD
         stack.append((i, n))
-    
+
     for i in range(len(stack)):
         j, n = stack[i]
         left = j - stack[i - 1][0] if i > 0 else j + 1
@@ -42,5 +43,6 @@ def sumSubarrayMins(arr: List[int]) -> int:
 
     return res
 
-print(sumSubarrayMins(arr = [3,1,2,4]))
-print(sumSubarrayMins(arr = [11,81,94,43,3]))
+
+print(sumSubarrayMins(arr=[3, 1, 2, 4]))
+print(sumSubarrayMins(arr=[11, 81, 94, 43, 3]))
