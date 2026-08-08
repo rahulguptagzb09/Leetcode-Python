8414751d29213fc0e330f0b473b18725d016695d
Rahul Gupta <rahulguptagzb09@gmail.com>
2026-08-08 11:44:44 +0530

    added 1 python solution for 763

--- medium/medium-0763-partition-labels.py
+++ medium/medium-0763-partition-labels.py
@@ -20,6 +20,7 @@ s consists of lowercase English letters.
 Hint 1
 Try to greedily choose the smallest partition that includes the first letter. If you have something like "abaccbdeffed", then you might need to add b. You can use an map like "last['b'] = 5" to help you expand the width of your partition.
 """
+
 # Time - O(n)
 # Space - O(1)
 
@@ -28,19 +29,21 @@ from typing import List
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        last_index = {} # char -> last index in s
+        # Greedy
+        last_index_map = {}  # char -> last index in s
         for i, c in enumerate(s):
-            last_index[c] = i
+            last_index_map[c] = i
         res = []
         size, end = 0, 0
         for i, c in enumerate(s):
             size += 1
-            end = max(end, last_index[c])
+            end = max(end, last_index_map[c])
             if i == end:
                 res.append(size)
                 size = 0
         return res
 
+
 sol = Solution()
-print(sol.partitionLabels(s = "ababcbacadefegdehijhklij"))
-print(sol.partitionLabels(s = "eccbbbbdec"))
+print(sol.partitionLabels(s="ababcbacadefegdehijhklij"))
+print(sol.partitionLabels(s="eccbbbbdec"))
